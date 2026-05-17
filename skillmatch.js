bac11e8c9afe9a243b0b337920eb81ab165592c5
Alessandra <adkrob2@gmail.com>
2026-05-17 20:53:05 -0300

    feat: cria objeto candidato
    Adiciona objeto candidato inicial.

---
+++ skillmatch.js
@@ -0,0 +1,9 @@
+// ======================================
+// OBJETO CANDIDATO
+// ======================================
+const candidato = {
+    nome:"Alessandra",
+    area:"Front-End",
+    habilidades: ["JavaScript","GitHub","lógica de programação", "Kanban"],
+    experienciaMeses: 3
+};
