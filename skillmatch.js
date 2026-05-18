b66ecc8c988eecca41196aa3ae1cd5f8c22aa050
Alessandra <adkrob2@gmail.com>
2026-05-17 21:45:14 -0300

    fix: adiciona campo empresa no array de vagas
    Adiciona o campo empresa no array de vagas que estava faltando.

--- skillmatch.js
+++ skillmatch.js
@@ -23,6 +23,7 @@ const vagas = [
     },
     {
         id:2,
+        empresa: "Far Way",
         cargo:"Estágio Front-End",
         requisitos:["JavaScript","HTML","CSS"],
         salario:1500,
@@ -30,6 +31,7 @@ const vagas = [
     },
     {
         id:3,
+        empresa: "Espotifai",
         cargo:"Programador JavaScript Júnior",
         requisitos:["Lógica de Programação","JavaScript","HTML","CSS","GitHub","Kanban", "POO"],
         salario:3000,
